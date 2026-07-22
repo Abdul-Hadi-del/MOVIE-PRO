75d7ffe623d48061ce7c15cc031633dd7f73d659
Abdul Hadi <hadi.akram557@gmail.com>
2026-07-22 07:44:57 +0500

    Point frontend to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import { auth } from "./firebase";
 
-const API_BASE = "http://127.0.0.1:5000";
+const API_BASE = "https://movie-pro-production.up.railway.app";
 
 async function authHeaders() {
   const token = await auth.currentUser.getIdToken();
